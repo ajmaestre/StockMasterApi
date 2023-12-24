f6328876a7d1624a3edfe6b1bf6564554a7f1569
ajmaestre <ajmaestretorres@gmail.com>
2023-12-23 21:31:12 -0500

    Subiendo primera versión StockMasterApi 1.0.0 - Cambiando Dockerfile 3

--- src/main/java/com/engineerds/stockmaster/utilities/Config.java
+++ src/main/java/com/engineerds/stockmaster/utilities/Config.java
@@ -2,7 +2,7 @@ package src.main.java.com.engineerds.stockmaster.utilities;
 
 public class Config {
 	
-	public static final String DB_HOST = "jdbc:mysql://localhost/";
+	public static final String DB_HOST = "jdbc:mysql://mysql-container:3306/";
     public static final String DB_USER = "root";
     public static final String DB_PASSWORD = "ADNarn-1993";
     public static final String DB_NAME = "stockmasterDB";
